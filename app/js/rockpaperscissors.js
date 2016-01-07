591d6b56048419cec7a7a11b2bf790053d8da0c9
Shawn Alwani <Shawn Alwani>
2016-01-08 01:57:48 +0800

    Added functions to play game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,12 +22,12 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    var move = move||'getInput()';
+    move = move||getInput();
     return move;
 }
 
 function getComputerMove(move) {
-    var move = move||'randomPlay()';
+    move = move||randomPlay();
     return move;
 }
 
@@ -55,8 +55,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins<5 && computerWins<5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var wins = getWinner(playerMove,computerMove);
+        if (wins==='player') {
+            playerWins +=1;
+        }
+        else if (wins==='computer') {
+            computerWins +=1;
+        }
+        else console.log('This round was a tie');
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
 
